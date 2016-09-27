91790e04ddad47cc054feecbab397eea419da0ee
Krzysztof Bober <krzysztof.bober@getbase.com>
2016-09-27 16:59:48 +0200

    using my own client to fetch data from last.fm

--- Gemfile
+++ Gemfile
@@ -1,1 +1,3 @@
+source "https://rubygems.org"
+
 gem "my_last_fm_client", :git => 'git@github.com:krizo/lastfm-client.git'
--- tracks.rb
+++ tracks.rb
@@ -1,66 +1,13 @@
-require 'CSV'
-require 'pry'
-require 'httparty'
+require 'last_fm_client'
+require 'exporter'
 require 'psych'
-
 
 config = Psych.load_file('scrobbles.yml')
 
 user = config['last_fm']['user']
-KEY = config['last_fm']['api_key']
-page = 0
-total = 1485 # total no of pages
+api_key = config['last_fm']['api_key']
 
-def get_track_info(artist, track_name)
-  artist.gsub!(" ", '+')
-  track_name.gsub!(" ", '+')
-  url = "http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=#{KEY}&format=json&artist=#{artist}&track=#{track_name}"
-  track_info = HTTParty.get(URI.escape(url))
-  if track_info['message'] == "Track not found"
-    nil
-  else
-    track_info
-  end
-end
-
-CSV.open("#{user}.csv", "wb") do |csv|
-  while page < total
-    url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=#{user}&api_key=#{KEY}&format=json&extended=1"
-    response = HTTParty.get(url)
-    response['recenttracks']['track'].each do |track|
-      track_name = track['name']
-      artist = track['artist']['name']
-      album = track['album']['#text']
-      date = track['date']
-      timestamp = date ? date['uts'] : nil
-      datestamp = date ? date['#text'] : nil
-      track_info = get_track_info(artist, track_name)
-      if track_info
-        duration = track_info['track']['duration']
-        image = if track_info['track']['album'].nil?
-          nil
-        else
-          track_info['track']['album']['image'].first['#text']
-        end
-        taggings = []
-        track_info['track']['toptags']['tag'].each do |tag|
-          taggings << tag['name'] unless tag.empty?
-        end
-        tags = taggings.join(";") if taggings
-      else
-        duration, image, tags = nil
-      end
-      csv << [
-        user,
-        timestamp,
-        datestamp,
-        artist,
-        track_name,
-        album,
-        duration,
-        image,
-        tags
-      ]
-    end
-  end
-end
+last_fm_client = LastfmClient.new(api_key, user)
+csv_file = "#{last_fm_client.user}.csv"
+exporter = CsvExporter.new(last_fm_client, csv_file)
+exporter.export_recent_tracks(csv_file, { start_page: 3, end_page: 3 })
