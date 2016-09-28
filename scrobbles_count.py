c199aa6fdc79d511b3d97dac991bddde974ceec3
Krzysztof Bober <krzysztof.bober@getbase.com>
2016-09-28 23:56:03 +0200

    simple script to calculate scrobbles by artist

---
+++ scrobbles_count.py
@@ -0,0 +1,21 @@
+from pyspark import SparkConf, SparkContext
+import collections
+import io
+import sys
+import operator
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+input_file = sys.argv[1]
+
+conf = SparkConf().setMaster("local").setAppName("Scrobbles")
+sc = SparkContext(conf = conf)
+
+lines = sc.textFile(input_file)
+ratings = lines.map(lambda x: x.split(',')[4])
+result = ratings.countByValue()
+
+sortedResults = collections.OrderedDict(sorted(result.items(), key=operator.itemgetter(1)))
+for key, value in sortedResults.items():
+    print("%s %i" % (key, value))
