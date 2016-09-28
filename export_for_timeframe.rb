cefa71aaaea81cb4813efb7adb665283c1476a99
Krzysztof Bober <krzysztof.bober@getbase.com>
2016-09-28 23:43:44 +0200

    changing script params

--- export_for_timeframe.rb
+++ export_for_timeframe.rb
@@ -8,8 +8,10 @@ user = config['last_fm']['user']
 api_key = config['last_fm']['api_key']
 
 last_fm_client = LastfmClient.new(api_key, user)
-start_time = Time.parse("2009-01-01 21:00:00 UTC")
-end_time = Time.parse("2013-05-04 20:57:00 UTC")
-csv_file = "#{last_fm_client.user}_#{end_time.to_date}-#{start_time.to_date}.csv"
-exporter = CsvExporter.new(last_fm_client, csv_file)
-exporter.export_for_timeframe(csv_file, start_time, end_time)
+# start_time = Time.parse("2009-01-01 21:00:00 UTC")
+# end_time = Time.parse("2013-05-04 20:57:00 UTC")
+start_time = Time.parse(ARGV[0])
+end_time = Time.parse(ARGV[1])
+output_file = ARGV[2] || "#{last_fm_client.user}_#{end_time.to_date}-#{start_time.to_date}.csv"
+exporter = CsvExporter.new(last_fm_client, output_file)
+exporter.export_for_timeframe(output_file, start_time, end_time)
