77c45a06c28dc26abb4e18be5c294aab1b51076d
Krzysztof Bober <krzysztof.bober@getbase.com>
2016-09-29 17:19:01 +0200

    counting play duration by artist

---
+++ data_pipelines/duration_by_artist.py
@@ -0,0 +1,31 @@
+from pyspark import SparkConf, SparkContext
+import collections
+import io
+import sys
+import operator
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def parseLine(line):
+    fields = line.split(',')
+    artist = fields[4].encode('utf8')
+    duration = int(fields[7]) / 1000
+    if duration == 0:
+        duration = 180
+    return (artist, duration)
+
+input_file = sys.argv[1]
+conf = SparkConf().setMaster("local").setAppName("DurationByArtist")
+sc = SparkContext(conf = conf)
+
+lines = sc.textFile(input_file)
+rdd = lines.map(parseLine)
+
+durations = rdd.reduceByKey(lambda x, y: (x + y) )
+for x in sorted(durations.collect(), key=operator.itemgetter(1)):
+    artist = x[0]
+    duration = x[1]
+    m, s = divmod(duration, 60)
+    h, m = divmod(m, 60)
+    print "%s:  %dh:%02dm:%02ds" % (artist, h, m, s)
